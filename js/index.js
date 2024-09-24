11c4d9942bd92dbfda395f88b7a67100bb51f714
marufahmedinfo <marufahmedinfos@gmail.com>
2024-09-24 14:37:47 +0600

    added a donation add js function

--- js/index.js
+++ js/index.js
@@ -15,14 +15,13 @@
 
 
 const giveNoakali = getTextFieldById("give-noakali");
-  
 const givefani = getTextFieldById("give-fani");
-// const giveQuota = getTextFieldById("give-quota");
+const giveQuota = getTextFieldById("give-quota");
 
-// const inputquota=getInputFieldValueById("q-input-donation");
+const total = getTextFieldById("total-balance");
  
   
-const total = getTextFieldById("total-balance");
+
   document.getElementById('noakali-donate-btn').addEventListener("click", function(){
      const inputNoakali = getInputFieldValueById("n-input-donation");
      if(isNaN !== inputNoakali && inputNoakali <= total){
@@ -37,7 +36,7 @@ const total = getTextFieldById("total-balance");
        alert('please type valid input')
        document.getElementById('n-input-donation').value='';
      }
-  })
+  });
 
 
   document.getElementById('fani-donate-btn').addEventListener("click",function(){
@@ -47,24 +46,28 @@ const total = getTextFieldById("total-balance");
      if(isNaN!==inputfani && inputfani <= total){
        const donationFani = inputfani + givefani;
        document.getElementById('give-fani').innerText = donationFani;
-       const newbalance =total - donationFani ;
+       const newbalance = total - donationFani ;
        document.getElementById('total-balance').innerText = newbalance;
        document.getElementById('f-input-donation').value='';
      }else{
       alert('please provide a valid input');
       document.getElementById('f-input-donation').value='';
      }
-  })
+  });
 
-// document.getElementById('qouta-donate-btn').addEventListener('click', function(){
-//     const inputquota = getElementById('q-input-donation');
-//     if(isNaN !== inputquota && inputquota <= total){
-//         const donation = inputquota + giveQuota;
-//         document.getElementById('give-quota').innerText = donation;
-//         const newbalance = total - donation;
-//         document.getElementById('total-balance').innerText = newbalance;
-//         document.getElementById('q-input-donation').value='';
-//     }
-// })
+document.getElementById('qouta-donate-btn').addEventListener('click', function(){
+    const inputquota = getInputFieldValueById("q-input-donation");
+
+    if(isNaN !== inputquota && inputquota <= total){
+        const donationQouta = inputquota + giveQuota;
+        document.getElementById('give-quota').innerText = donationQouta;
+        const newbalance = total - donationQouta;
+        document.getElementById('total-balance').innerText = newbalance;
+        document.getElementById('q-input-donation').value='';
+    }else{
+      alert('please provide a valid input');
+      document.getElementById('q-input-donation').value='';
+    }
+})
 
 
